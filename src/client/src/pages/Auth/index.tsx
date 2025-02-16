4a77248978873599b6ab9fd874bc4445d84cb545
Maximus7474 <the.maximus.7474@gmail.com>
2025-02-16 12:47:19 +0100

    feat(client/pages/Auth): base login & register form -> no calls to backend yet

--- src/client/src/pages/Auth/index.tsx
+++ src/client/src/pages/Auth/index.tsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import LoginForm from "./login-form";
+import RegisterForm from "./register-form";
+
 const AuthPage = () => {
-    return <h1>AuthPage</h1>
-}
+    const [displayForm, setDisplayForm] = useState<"login" | "register">("login");
+
+    return (
+        <div className="flex items-center justify-center min-h-screen w-full p-4">
+            <div className="w-full max-w-sm md:max-w-3xl">
+                {displayForm === "login" && (
+                    <LoginForm changeForm={() => setDisplayForm("register")} />
+                )}
+                {displayForm === "register" && (
+                    <RegisterForm changeForm={() => setDisplayForm("login")} />
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default AuthPage;
