49db12834a6a684a181087fa6072d152779588da
Maximus7474 <the.maximus.7474@gmail.com>
2025-02-16 18:43:16 +0100

    refactor(pages/Auth): implement figma designs to form components

--- src/client/src/pages/Auth/login-form.tsx
+++ src/client/src/pages/Auth/login-form.tsx
@@ -12,43 +12,47 @@ interface LoginFormProps extends React.ComponentProps<"div"> {
 const LoginForm = ({ changeForm, className, ...props }: LoginFormProps) => {
     return (
         <div className={cn("flex flex-col gap-6", className)} {...props}>
-            <Card className="overflow-hidden">
+            <Card className="overflow-hidden border-transparent md:border-primary">
                 <CardContent className="grid p-0 md:grid-cols-2">
                     <form className="p-6 md:p-8">
-                        <div className="flex flex-col gap-6">
+                        <div className="flex flex-col gap-8">
                             <div className="flex flex-col items-center text-center">
-                                <h1 className="text-2xl font-bold">Welcome back</h1>
-                                <p className="text-balance text-muted-foreground">
-                                    Login to your NutriFit account
-                                </p>
+                                <h2 className="text-2xl font-normal text-secondary">Welcome back to</h2>
+                                <h1 className="text-4xl font-semibold text-primary">NutrifFit</h1>
                             </div>
                             <div className="grid gap-2">
-                                <Label htmlFor="email">Email</Label>
+                                <Label htmlFor="email" className="text-primary">Email Address</Label>
                                 <Input
                                     id="email"
                                     type="email"
                                     placeholder="johndoe@nutrifit.com"
                                     required
+                                    className="border-[1.5px] border-solid rounded-3xl"
                                 />
                             </div>
                             <div className="grid gap-2">
                                 <div className="flex items-center">
-                                    <Label htmlFor="password">Password</Label>
+                                    <Label htmlFor="password" className="text-primary">Password</Label>
                                     <a
                                         href="#"
-                                        className="ml-auto text-sm underline-offset-2 hover:underline"
+                                        className="ml-auto text-sm underline-offset-2 hover:underline text-primary"
                                     >
                                         Forgot your password?
                                     </a>
                                 </div>
-                                <Input id="password" type="password" required />
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    required
+                                    className="border-[1.5px] border-solid rounded-3xl"
+                                />
                             </div>
-                            <Button type="submit" className="w-full">
+                            <Button type="submit" className="w-fit px-[2em] rounded-3xl mx-auto">
                                 Login
                             </Button>
                             <div className="text-center text-sm">
                                 Don&apos;t have an account?{" "}
-                                <a className="underline underline-offset-4" onClick={changeForm}>
+                                <a className="underline underline-offset-4 cursor-pointer hover:underline-offset-2 hover:text-primary" onClick={changeForm}>
                                     Sign up
                                 </a>
                             </div>
@@ -63,7 +67,7 @@ const LoginForm = ({ changeForm, className, ...props }: LoginFormProps) => {
                     </div>
                 </CardContent>
             </Card>
-            <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+            <div className="text-balance text-center text-xs text-muted-foreground hover:[&_a]:text-secondary hover:[&_a]:underline">
                 By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
                 and <a href="#">Privacy Policy</a>.
             </div>
--- src/client/src/pages/Auth/register-form.tsx
+++ src/client/src/pages/Auth/register-form.tsx
@@ -12,7 +12,7 @@ interface LoginFormProps extends React.ComponentProps<"div"> {
 const LoginForm = ({ changeForm, className, ...props }: LoginFormProps) => {
     return (
         <div className={cn("flex flex-col gap-6", className)} {...props}>
-            <Card className="overflow-hidden">
+            <Card className="overflow-hidden border-transparent md:border-primary">
                 <CardContent className="grid p-0 md:grid-cols-2">
                     <div className="relative hidden bg-muted md:block">
                         <img
@@ -24,38 +24,46 @@ const LoginForm = ({ changeForm, className, ...props }: LoginFormProps) => {
                     <form className="p-6 md:p-8">
                         <div className="flex flex-col gap-6">
                             <div className="flex flex-col items-center text-center">
-                                <h1 className="text-2xl font-bold">Welcome back</h1>
-                                <p className="text-balance text-muted-foreground">
-                                    Sign Up now to join NutriFit
-                                </p>
+                                <h2 className="text-2xl font-normal text-secondary">Welcome to</h2>
+                                <h1 className="text-4xl font-semibold text-primary">NutrifFit</h1>
                             </div>
                             <div className="grid gap-2">
-                                <Label htmlFor="email">Email</Label>
+                                <Label htmlFor="name" className="text-primary">Name</Label>
+                                <Input
+                                    id="name"
+                                    type="name"
+                                    placeholder="John Doe"
+                                    required
+                                    className="border-[1.5px] border-solid rounded-3xl"
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="email" className="text-primary">Email</Label>
                                 <Input
                                     id="email"
                                     type="email"
                                     placeholder="johndoe@nutrifit.com"
                                     required
+                                    className="border-[1.5px] border-solid rounded-3xl"
                                 />
                             </div>
                             <div className="grid gap-2">
                                 <div className="flex items-center">
-                                    <Label htmlFor="password">Password</Label>
-                                    <a
-                                        href="#"
-                                        className="ml-auto text-sm underline-offset-2 hover:underline"
-                                    >
-                                        Forgot your password?
-                                    </a>
+                                    <Label htmlFor="password" className="text-primary">Password</Label>
                                 </div>
-                                <Input id="password" type="password" required />
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    required
+                                    className="border-[1.5px] border-solid rounded-3xl"
+                                />
                             </div>
-                            <Button type="submit" className="w-full">
-                                Login
+                            <Button type="submit" className="w-fit px-[2em] rounded-3xl mx-auto">
+                                Sign Up
                             </Button>
                             <div className="text-center text-sm">
                                 Have an account?{" "}
-                                <a className="underline underline-offset-4" onClick={changeForm}>
+                                <a className="underline underline-offset-4 cursor-pointer hover:underline-offset-2 hover:text-primary" onClick={changeForm}>
                                     Login
                                 </a>
                             </div>
@@ -63,9 +71,9 @@ const LoginForm = ({ changeForm, className, ...props }: LoginFormProps) => {
                     </form>
                 </CardContent>
             </Card>
-            <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
-                By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-                and <a href="#">Privacy Policy</a>.
+            <div className="text-balance text-center text-xs text-muted-foreground hover:[&_a]:text-secondary hover:[&_a]:underline">
+                By signing up you agree to our <a href="#">Terms of Service</a>{" "}
+                and our <a href="#">Privacy Policy</a>.
             </div>
         </div>
     )
