24b65073e701e17540c85f9fa44c6e80bd77fdb2
Maximus7474 <the.maximus.7474@gmail.com>
2025-02-18 19:17:42 +0100

    tweak(server/config): inform in console if env variables failed to load or if none where found

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,4 +1,10 @@
-require("dotenv").config();
+const result = require("dotenv").config();
+
+if (result.error) {
+    console.warn(`[WARN] Unable to to load .env file ${result.error.message || 'no details available'} defaulting to base values`)
+} else if (Object.keys(result.parsed) === 0) {
+    console.warn('[WARN] No keys found in the .env file, the system will default to fallback values');
+}
 
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
