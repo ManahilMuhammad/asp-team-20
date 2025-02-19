fb7a99e301e0d3fe0bd69be11f843614a74f1a13
Maximus7474 <the.maximus.7474@gmail.com>
2025-02-19 14:41:38 +0100

    feat(server/config): added validation of JWT Secret in .env

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -24,7 +24,32 @@ if (
 ) PORT = env.PORT;
 else console.warn(`[WARN] PORT was incorrectly set in the .env file to ${env.NODE_ENV}, defaulting to 3000`)
 
+const generateRandomString = (length) => {
+    if (!length || typeof length !== 'number' || length < 0 || length > 50) length = 20;
+
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters[randomIndex];
+    }
+
+    return result;
+};
+
+let JWT_SECRET;
+if (
+    typeof env.JWT_SECRET === 'string' &&
+    env.JWT_SECRET.length >= 10
+) JWT_SECRET = env.JWT_SECRET;
+else {
+    console.warn('[WARN] no valid JWT_SECRET is provided, generating a new one of length 20');
+    JWT_SECRET = generateRandomString(20);
+}
+
 module.exports = {
     NODE_ENV,
     PORT,
+    JWT_SECRET,
 }
