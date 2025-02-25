4043e40d822e86eb48cf9e009012c64caf8dc2e1
Maximus7474 <the.maximus.7474@gmail.com>
2025-02-26 00:06:04 +0100

    chore(client/App): implement protected route for profile page

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -3,13 +3,14 @@ import './App.css'
 import HomePage from "./pages/Home";
 import ProfilePage from "./pages/Profile";
 import AuthPage from "./pages/Auth";
+import ProtectedRoute from "./components/protected-route";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={<ProtectedRoute children={<ProfilePage />} />} />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>
     </Router>
