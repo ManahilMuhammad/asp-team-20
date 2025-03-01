a12af84a753eb860324ba5e665bac7921f884568
Maximus7474 <the.maximus.7474@gmail.com>
2025-03-01 15:10:52 +0100

    feat(client/App): routing for recipe pages

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -5,6 +5,12 @@ import ProfilePage from "./pages/Profile";
 import AuthPage from "./pages/Auth";
 import ProtectedRoute from "./components/protected-route";
 
+import RecipePage from "./pages/Recipes/main";
+import CreateRecipe from "./pages/Recipes/Create/main";
+import SavedRecipes from "./pages/Recipes/Saved/main";
+import SearchRecipes from "./pages/Recipes/SearchRecipes/main";
+import SuggestedRecipes from "./pages/Recipes/Suggested/main";
+
 const App = () => {
   return (
     <Router>
@@ -12,6 +18,15 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/profile" element={<ProtectedRoute children={<ProfilePage />} />} />
         <Route path="/auth" element={<AuthPage />} />
+
+        <Route path="/recipes" element={<RecipePage />}>
+          <Route index element={<>Parent</>} />
+          <Route path="create" element={<ProtectedRoute children={<CreateRecipe />} />} />
+          <Route path="saved" element={<ProtectedRoute children={<SavedRecipes />} />} />
+          <Route path="search" element={<SearchRecipes />} />
+          <Route path="suggestions" element={<SuggestedRecipes />} />
+        </Route>
+
       </Routes>
     </Router>
   );
