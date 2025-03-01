fd0c4aba2345601bbc74bdc733a7fb5303284354
Maximus7474 <the.maximus.7474@gmail.com>
2025-03-01 19:22:12 +0100

    refactor(client/pages/Recipes): added base fetching to get suggestions from server endpoint - code is present but commented as endpoint doesn't exist - to test visuals and tweak it use the placeholderRecipes as recipe suggestion list

--- src/client/src/pages/Recipes/Suggested/main.tsx
+++ src/client/src/pages/Recipes/Suggested/main.tsx
@@ -1,46 +1,54 @@
 import RecipeCard, { RecipeData } from "@/components/recipe-card";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { useState } from "react";
+// import useFetchWithAuth from "@/hooks/use-fetch-api";
+import { Loader2, TriangleAlert } from "lucide-react";
+import { useEffect, useState } from "react";
 
-const placeholderRecipes: RecipeData[] = [
-    {
-        name: "Chicken Parmesan",
-        icon: "https://www.mamaknowsglutenfree.com/wp-content/uploads/2023/06/gluten-free-chicken-parmesan-rc-1.jpg",
-        tags: [
-            { label: "Nut Free", color: "red" },
-            { label: "Keto", color: "blue" },
-        ],
-    },
-    {
-        name: "Avocado Toast",
-        icon: "https://veganhuggs.com/wp-content/uploads/2023/02/white-bean-avocado-toast.jpg",
-        tags: [
-            { label: "Vegan", color: "green" },
-            { label: "Gluten-Free", color: "purple" },
-        ],
-    },
-    {
-        name: "Salmon Teriyaki",
-        icon: "https://rasamalaysia.com/wp-content/uploads/2016/03/salmon-teriyaki-thumb.jpg",
-        tags: [
-            { label: "High Protein", color: "orange" },
-            { label: "Omega-3", color: "blue" },
-        ],
-    },
-    {
-        name: "Quinoa Salad",
-        icon: "https://www.crowdedkitchen.com/wp-content/uploads/2022/10/Pumpkin-Quinoa-Salad-11.jpg",
-        tags: [
-            { label: "Vegan", color: "green" },
-            { label: "Gluten-Free", color: "purple" },
-            { label: "Low Carb", color: "yellow" },
-        ],
-    },
-];
+// const placeholderRecipes: RecipeData[] = [
+//     {
+//         name: "Chicken Parmesan",
+//         icon: "https://www.mamaknowsglutenfree.com/wp-content/uploads/2023/06/gluten-free-chicken-parmesan-rc-1.jpg",
+//         tags: [
+//             { label: "Nut Free", color: "red" },
+//             { label: "Keto", color: "blue" },
+//         ],
+//     },
+//     {
+//         name: "Avocado Toast",
+//         icon: "https://veganhuggs.com/wp-content/uploads/2023/02/white-bean-avocado-toast.jpg",
+//         tags: [
+//             { label: "Vegan", color: "green" },
+//             { label: "Gluten-Free", color: "purple" },
+//         ],
+//     },
+//     {
+//         name: "Salmon Teriyaki",
+//         icon: "https://rasamalaysia.com/wp-content/uploads/2016/03/salmon-teriyaki-thumb.jpg",
+//         tags: [
+//             { label: "High Protein", color: "orange" },
+//             { label: "Omega-3", color: "blue" },
+//         ],
+//     },
+//     {
+//         name: "Quinoa Salad",
+//         icon: "https://www.crowdedkitchen.com/wp-content/uploads/2022/10/Pumpkin-Quinoa-Salad-11.jpg",
+//         tags: [
+//             { label: "Vegan", color: "green" },
+//             { label: "Gluten-Free", color: "purple" },
+//             { label: "Low Carb", color: "yellow" },
+//         ],
+//     },
+// ];
 
 const SuggestedRecipes = () => {
 
-    const [recipes, setSuggestedRecipes] = useState<RecipeData[]>(placeholderRecipes);
+    const [recipes, setSuggestedRecipes] = useState<RecipeData[]>([]);
+
+    const { data, loading, error } = { data: null, loading: false, error: 'No current API endpoint to suggest recipes\nManually set recipe suggestions in ./src/pages/Recipes/Suggested/main.tsx to test the component.' };//useFetchWithAuth<RecipeData[]>("/api/recipes");
+
+    useEffect(() => {
+        if (data && !loading && !error) setSuggestedRecipes(data);
+    }, [data, loading, error]);
 
     return <div className="px-4 pt-16">
         <h1 className="text-2xl font-medium text-nutrifit-tertiary text-center">Suggested Recipes</h1>
@@ -48,6 +56,21 @@ const SuggestedRecipes = () => {
     
         <ScrollArea className="w-full mt-4">
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 mt-4 max-w-[90vw] mx-auto">
+
+                {
+                    loading && !error && <div className="mx-auto flex items-center gap-2">
+                        <Loader2 className="animate-spin" /> Loading Recipes
+                    </div>
+                }
+
+                {
+                    error && <div className="mx-auto text-center">
+                        <p className="flex items-center mb-2">
+                            <TriangleAlert className="mr-2 text-red-500"/> An error occured while obtaining the suggested recipes:
+                        </p>
+                        <span className="text-red-500">{error}</span>
+                    </div>
+                }
 
                 {
                     recipes.length > 0 && recipes.map((recipe, i) => (
