20e1552072b2835f166866780d49cb2fbae1f745
Maximus7474 <the.maximus.7474@gmail.com>
2025-03-04 11:47:56 +0100

    tweak(client/pages/Recipes/Create): placeholder text as no available design for the page Yet

--- src/client/src/pages/Recipes/Create/main.tsx
+++ src/client/src/pages/Recipes/Create/main.tsx
@@ -1,5 +1,31 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Cog, Construction, Drill } from "lucide-react";
+
 const CreateRecipe = () => {
-    return <h2>Create a New Recipe</h2>;
+    return (
+        <div className="flex flex-col items-center gap-8 mt-16">
+            <div className="flex flex-col lg:flex-row items-center gap-8">
+                <img src="/nutrifit-logo.svg" className="rounded-[25%]" alt="Nutrifit Logo" />
+                <h1 className="text-4xl md:text-6xl text-primary">Under Construction</h1>
+            </div>
+
+            <Card className="max-w-[80vw] md:max-w-[60vw] lg:max-w-[50vw]">
+                <CardHeader>
+                    <CardTitle>Recipe Creation</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <p>
+                        This section is still under development, please follow our social media to obtain more information on it's progress and release date.
+                    </p>
+                    <div className="flex flex-row items-center justify-center gap-4 mt-8">
+                        <Construction size={48} />
+                        <Cog size={48} className="animate-spin" />
+                        <Drill size={48} />
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
+    );
 };
 
 export default CreateRecipe;
