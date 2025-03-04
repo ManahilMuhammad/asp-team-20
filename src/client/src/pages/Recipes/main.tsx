b5728c6f2b5f0acacb63334ca5cacd31f94d798e
Maximus7474 <the.maximus.7474@gmail.com>
2025-03-04 13:18:51 +0100

    feat(client/pages/Recipes): added home button to return to Profile Page or HomeScreen -> depends if auth'ed or not

--- src/client/src/pages/Recipes/main.tsx
+++ src/client/src/pages/Recipes/main.tsx
@@ -1,10 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/hooks/use-auth";
+import { HomeIcon } from "lucide-react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const RecipePage = () => {
+    const navigate = useNavigate();
+    const { isAuthenticated } = useAuth();
+
     return <>
-        {/* <h2>Recipe Page</h2> */}
-        {/* Possible to add a navbar or menu to navigate between the subroutes */}
         <Outlet />
+        <div className="absolute bottom-[1em] left-1/2 -translate-x-1/2 z-[1000]">
+            <Button
+                className="rounded-full w-[3em] h-[3em] p-[0.4em] bg-[#FFAF01] hover:bg-[#c98a02] dark:hover:bg-[#ffd06b] shadow-xl"
+                onClick={() => {
+                    navigate(
+                        isAuthenticated() ? 
+                            '/profile' :
+                            '/'
+                        );
+                }}
+                asChild
+            >
+                <HomeIcon strokeWidth={1} size={48} className="text-[#424242]" />
+            </Button>
+        </div>
     </>;
 };
 
