f9f4fd43617da25435bc90f91d3c64b4006b3f9d
Maximus7474 <the.maximus.7474@gmail.com>
2025-03-08 23:51:56 +0100

    feat(client/utils): formatProfileImgUrl

--- src/client/src/lib/utils.ts
+++ src/client/src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatProfileImgUrl = (avatar: string): string => {
+  // avatar is a url reference to an online image
+  if (avatar.startsWith('http')) return avatar;
+  // default avatar being nutifit logo
+  if (avatar === 'default') return "/nutrifit-logo.svg";
+  // any other default avatar
+  return `/base-avatars/${avatar}`;
+} 
